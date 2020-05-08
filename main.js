7a53643443ca3ddb20ad7d2bf771b9f0d24d6bf8
Marc Juul <marc@juul.io>
2020-05-08 03:14:18 -0700

    Added XML parsing and status display

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const fs = require('fs-extra');
+const formatTime = require('timeago.js').format;
 const chokidar = require('chokidar');
 
 const SETTINGS_PATH = "./settings.json";
+
+var lastScanTime;
 
 function saveSettings(settings) {
   fs.writeFileSync(SETTINGS_PATH, {encoding: 'utf8'}, JSON.stringify(settings))
@@ -22,18 +25,6 @@ function loadSettings() {
 }
 
 const settings = loadSettings();
-
-function startWatching(watchPath) {
-  console.log("Watching:", watchPath);
-  const watcher = chokidar.watch(watchPath, {
-    persistent: true,
-    alwaysStat: true
-  })
-
-  watcher.on('add', function(filepath, stats) {
-    console.log("filepath:", filepath);
-  });
-}
 
 function gotoPage(pageName) {
   const pages = document.querySelectorAll('.page');
@@ -68,14 +59,115 @@ function initLinks() {
   }
   
 }
+function capitalize(str) {
+  if(!str) return;
+
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+function parseXML(str) {
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(str, 'application/xml');
+  
+  // Check for errors according to:
+  // https://developer.mozilla.org/en-US/docs/Web/API/DOMParser
+  var errs = doc.getElementsByTagName('parsererror');
+  if(errs.length) {
+    var txt = errs[0].textContent;
+    txt = txt.replace(/Below is a rendering.*/i, ''); // remove useless message
+    txt = txt.replace(':', ': ').replace(/\s+/, ' '); // improve formatting
+    throw new Error("Parsing XML failed: " + txt);
+  }
+  return doc;
+}
+
+function openXML(filepath, cb) {
+  fs.readFile(filepath, {encoding: 'utf8'}, function(err, data) {
+    if(err) return cb(err);
+    try {
+      const doc = parseXML(data);
+      cb(null, doc);
+    } catch(e) {
+      return cb(e);
+    }
+  });
+}
+
+function showError(err) {
+  var el = document.getElementById('error-msg');
+  el.innerHTML = err.toString();
+  el = document.querySelector('.error');
+  el.style.display = "block";
+
+}
+
+function updateLastScan(keepUpdating) {
+
+  var el = document.getElementById('last-scan-time');
+  if(!lastScanTime) {
+    el.innerHTML = "Never"
+  } else {
+    el.innerHTML = capitalize(formatTime(lastScanTime));
+  }
+  
+  if(keepUpdating) {
+    setTimeout(function() {
+      updateLastScan(keepUpdating);
+    }, keepUpdating);
+  }
+}
+
+
+
+function writeScanData(data) {
+  // TODO
+
+  lastScanTime = new Date();
+  updateLastScan();
+}
+
+function parseNewFile(filepath, numTries) {
+  openXML(filepath, function(err, doc) {
+    if(err) {
+      numTries = numTries || 0;
+      if(numTries >= 2) {
+        showError("Failed to read file: " + filepath);
+      }
+      setTimeout(function() {
+        parseNewFile(filepath, numTries+1);
+      }, 3000);
+      return;
+    }
+
+    const el = doc.querySelector('Root > Foo');
+    const data = el.innerHTML.trim();
+    console.log("Got:", data);
+    
+    writeScanData(data);
+  });
+}
+
+
+function startWatching(watchPath) {
+  console.log("Watching:", watchPath);
+  const watcher = chokidar.watch(watchPath, {
+    persistent: true,
+    alwaysStat: true
+  })
+
+  watcher.on('add', function(filepath, stats) {
+    console.log("filepath:", filepath);
+
+    // give the decapper software some time to write the file
+    setTimeout(function() {
+      parseNewFile(filepath);
+    }, 500);
+  });
+}
 
 if(settings.watchPath) {
   startWatching(settings.watchPath);
 }
-
-//var notification = new Notification("Syncronizing...",options);
-//var notification = new Notification("DONE");
-
  
 var win = nw.Window.get();
 win.moveTo(0, 0);
@@ -83,3 +175,6 @@ win.resizeTo(320, 240);
 win.setAlwaysOnTop(true);
 
 initLinks();
+
+updateLastScan(5000);
+
